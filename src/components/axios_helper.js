31482a7e465aab79aa83b67c36f26e7e4c61b6fa
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-08-05 19:39:15 +0530

    modified axios_helper.js in components

--- src/components/axios_helper.js
+++ src/components/axios_helper.js
@@ -1,5 +1,13 @@
 import React from "react";
+import axios from "axios";
 
-export default function axios_helper() {
-  return <div>axios</div>;
-}
+axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.headers.post["Content-Type"] = "applications/json";
+
+export const request = (method, url, data) => {
+  return axios({
+    method: method,
+    url: url,
+    data: data,
+  });
+};
